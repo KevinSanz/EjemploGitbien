ee687a50988fe5eeb708d939027bb3e6f946f75f
KevinSanz <kevinbm2007@hotmail.com>
2015-11-06 17:55:43 +0100

    Ejemplo 1
    en este metodo empiezo

---
+++ src/Ejemplo1.java
@@ -0,0 +1,16 @@
+import acm.graphics.*;
+
+public class Ejemplo1 extends acm.program.GraphicsProgram{
+	public void run(){
+	GLabel etiqueta=new GLabel("hola git!!");
+	add(etiqueta);
+	//cambio tamaño de la ventana
+	setSize(800,600);
+	//llamo al metodo que dibuja la piramide 
+	pintarPiramide();
+	}
+	public void pintarPiramide(){
+		
+	}
+
+}
